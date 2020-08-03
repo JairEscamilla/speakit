5a427bff41d30a2ae54d7682a41bf990e8d79d26
jairescamilla <hernandez_yo14@outlook.es>
2020-08-03 18:34:13 -0500

    Rendering card

--- speakit/frontend/src/components/users/Feed.vue
+++ speakit/frontend/src/components/users/Feed.vue
@@ -11,7 +11,33 @@
                     asd
                 </v-col>
                 <v-col cols="12" md="7" > 
-                    <NuevoPost @clicked="updatePosts"></NuevoPost>
+                    <NuevoPost @clicked="updatePosts" class="mb-10"></NuevoPost>
+                    <div v-for="(item, index) in posts" :key="index" class="mb-15">
+                        <v-card class="mx-auto" mx-width="90%">
+                            <v-card-text class="headline font-weight-bold">
+                                {{ item.post }}
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-list-item class="grow">
+                                    <v-list-item-avatar color="grey">
+                                        <v-img class="elevation-6"
+                                            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light">
+                                        </v-img>
+                                    </v-list-item-avatar>
+                                    <v-list-item-content>
+                                        <v-list-item-title>Username</v-list-item-title>
+                                    </v-list-item-content>
+                                    <v-row align="center" justify="end">
+                                        <v-icon class="mr-1">mdi-heart</v-icon>
+                                        <span class="subheading mr-2">256</span>
+                                        <span class="mr-1">·</span>
+                                        <v-icon class="mr-1">mdi-share-variant</v-icon>
+                                        <span class="subheading">45</span>
+                                    </v-row>
+                                </v-list-item>
+                            </v-card-actions>
+                        </v-card>
+                    </div>
                 </v-col>
                 <v-col cols="2" class="links">
                     NUevos links
@@ -19,13 +45,6 @@
             </v-row>
 
         </v-container>
-        <div >
-            <ul>
-                <li v-for="(item, index) in posts" :key="index">
-                    {{ item.post }}
-                </li>
-            </ul>
-        </div>
         <v-btn color="error" @click="logout">Logout</v-btn>
     </div>
 </template>
@@ -45,7 +64,6 @@
             NuevoPost
         },
         created() {
-            console.log(this.$store.state.user_id);
             axios.get(this.$store.state.api + 'posts/', {
                 headers: {
                     Authorization: 'Token ' + this.$store.state.token
