7fe5b15e102dc6ffeebb535905829d2fa98c00ad
jairescamilla <hernandez_yo14@outlook.es>
2020-08-06 18:27:42 -0500

    Validating that the current user cannot follow himself

--- speakit/frontend/src/components/SearchResults.vue
+++ speakit/frontend/src/components/SearchResults.vue
@@ -1,8 +1,49 @@
 <template>
     <div>
-        Resultados de la búsqueda
-        {{ this.query }}
-        {{this.search}}
+
+        <v-container>
+            <v-row no-glutters>
+                <v-col cols="2" class="links">
+                    Links
+                </v-col>
+
+                <v-col cols="12" md="7">
+                    <h3>
+                        Results for: {{ this.query }}
+                    </h3>
+                    <div class="users-container">
+                        <div v-for="(user, index) in results" :key="index">
+                            <v-card max-width="90%" class="mx-auto">
+                                <v-list-item>
+                                    <v-list-item-avatar color="grey"></v-list-item-avatar>
+                                    <v-list-item-content>
+                                        <v-list-item-title class="headline">{{ user.first_name }} {{ user.last_name }}</v-list-item-title>
+                                        <v-list-item-subtitle>@{{user.username}}</v-list-item-subtitle>
+                                    </v-list-item-content>
+                                </v-list-item>
+                    
+                                <v-card-text>
+                                    "Profile description"
+                                </v-card-text>
+                    
+                                <v-card-actions class="actions mb-4">
+                                    <v-btn text color="deep-purple accent-4">
+                                        View profile
+                                    </v-btn>
+                                    <v-btn color="error" class="ml-4 mr-5" v-bind:disabled="username == user.username">Follow</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </div>
+                    </div>
+                </v-col>
+
+                <v-col cols="2" class="links">
+                    Links
+                </v-col>
+            </v-row>
+        </v-container>
+
+        
     </div>
 </template>
 
@@ -11,7 +52,7 @@
         data() {
             return {
                 results: [],
-                search: "asd"
+                username: this.$store.state.username
             }
         },
         props: {
@@ -19,6 +60,7 @@
         },
         created() {
             this.getUsers(this.query)
+            console.log(this.username);
         },
         updated() {
             this.getUsers(this.query)
@@ -34,8 +76,7 @@
                     body: data 
                 }).then((response) => response.json())
                 .then((data) => {
-                    console.log("Respuesta del servidor");
-                    console.log(data);
+                    this.results = data
                 })
             }
         },
